80ba0d96531497bcf0d906e4352b7361bd8f41e3
sulv <sulv1218@gmail.com>
2022-08-22 02:05:07 +0800

    [opt]完善个人界面

--- module_personal/src/main/java/com/qxy/module/personal/ui/personal/PersonalFragment.kt
+++ module_personal/src/main/java/com/qxy/module/personal/ui/personal/PersonalFragment.kt
@@ -2,7 +2,7 @@ package com.qxy.module.personal.ui.personal
 
 import android.os.Bundle
 import android.view.View
-import androidx.core.view.isGone
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.lifecycleScope
 import androidx.viewpager2.adapter.FragmentStateAdapter
@@ -14,33 +14,45 @@ import com.qxy.lib.base.base.network.whenLoading
 import com.qxy.lib.base.base.network.whenNone
 import com.qxy.lib.base.base.network.whenSuccess
 import com.qxy.lib.base.base.view.fragment.BaseVmBindFragment
+import com.qxy.lib.base.ext.log
+import com.qxy.lib.base.ext.setLightStatusBar
 import com.qxy.lib.base.ext.toast
 import com.qxy.lib.common.config.RouteTable
 import com.qxy.module.personal.databinding.FragmentPersonalBinding
 import com.qxy.module.personal.ui.fanfollow.PersonalFanFollowActivity
+import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.flow.collectLatest
 
 @Route(path = RouteTable.PERSONAL_ENTRY)
+@AndroidEntryPoint
 class PersonalFragment : BaseVmBindFragment<PersonalViewModel, FragmentPersonalBinding>() {
 
     private val tabName = arrayOf("作品", "私密", "收藏", "喜欢")
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+//        Thread.sleep(3000L)
+        setupStatusBar()
         initView()
         flowObserve()
     }
 
+    override fun onResume() {
+        super.onResume()
+        viewModel.initialize()
+    }
+
     private fun initView() {
         binding.personalTvFansInfo.setOnClickListener {
-            PersonalFanFollowActivity.startFanFollowActivity(requireActivity(), 0)
-        }
-        binding.personalTvFansData.setOnClickListener {
-            PersonalFanFollowActivity.startFanFollowActivity(requireActivity(), 0)
-        }
-        binding.personalTvFollowInfo.setOnClickListener {
+            binding.personalTvFollowInfo.setOnClickListener {
+                log { "click follow" }
+                PersonalFanFollowActivity.startFanFollowActivity(requireActivity(), 0)
+            }
+            binding.personalTvFollowData.setOnClickListener {
+                PersonalFanFollowActivity.startFanFollowActivity(requireActivity(), 0)
+            }
             PersonalFanFollowActivity.startFanFollowActivity(requireActivity(), 1)
         }
-        binding.personalTvFollowData.setOnClickListener {
+        binding.personalTvFansData.setOnClickListener {
             PersonalFanFollowActivity.startFanFollowActivity(requireActivity(), 1)
         }
         binding.vpPersonal.adapter = object : FragmentStateAdapter(this) {
@@ -55,26 +67,27 @@ class PersonalFragment : BaseVmBindFragment<PersonalViewModel, FragmentPersonalB
         }
         TabLayoutMediator(binding.tlPersonal, binding.vpPersonal) { tab, pos ->
             tab.text = tabName[pos]
-        }
+        }.attach()
     }
 
     private fun flowObserve() {
         viewLifecycleOwner.lifecycleScope.launchWhenStarted {
             viewModel.personal.collectLatest { result ->
 
+                // 没有相关接口，先写死
                 // 没有简介学校，暂时隐藏
-                binding.personalTvDesc.isGone = true
-                binding.tvSchool.isGone = true
+//                binding.personalTvDesc.isGone = true
+//                binding.tvSchool.isGone = true
 
                 result.whenSuccess { info ->
                     Glide.with(binding.root).load(info.avatar).into(binding.personalIvUserAvatar)
                     binding.personalTvName.text = info.nickname
-                    binding.personalTvDouyinNum.text = "抖音号：${info.openID}"
-                    binding.tvGender.text = when (info.gender) {
-                        1 -> "♂ 未知"
-                        2 -> "♀ 未知"
-                        else -> "未知"
-                    }
+//                    binding.personalTvDouyinNum.text = "抖音号：${info.openID}"
+//                    binding.tvGender.text = when (info.gender) {
+//                        1 -> "♂ 未知"
+//                        2 -> "♀ 未知"
+//                        else -> "未知"
+//                    }
                     return@whenSuccess
                 }
                 result.whenFailure { e ->
@@ -88,6 +101,17 @@ class PersonalFragment : BaseVmBindFragment<PersonalViewModel, FragmentPersonalB
                     return@whenNone
                 }
             }
+        }
+    }
+
+    private fun setupStatusBar() {
+        requireActivity().window.statusBarColor =
+            ContextCompat.getColor(requireActivity(), com.qxy.lib.common.R.color.transparent)
+        with(requireActivity()) {
+            window.statusBarColor = ContextCompat.getColor(
+                this, com.qxy.lib.common.R.color.transparent
+            )
+            setLightStatusBar()
         }
     }
 }
